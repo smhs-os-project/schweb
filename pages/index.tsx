dc21fce861cad0ab74f8a7f49367592b0362626d
pan93412 <pan93412@gmail.com>
2020-05-06 20:11:25 +0800

    feat(pages/home): done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,45 @@
-import Head from 'next/head';
 import React from 'react';
-import {
-  Section, Container, Title, Subtitle,
-} from 'bloomer';
+import { Columns } from 'bloomer/lib/grid/Columns';
+import { Column } from 'bloomer/lib/grid/Column';
+import { GetStaticProps } from 'next';
+import Parser from 'rss-parser';
+import { Title, Section } from 'bloomer';
+import Navbar from '../component/navbar';
+import Announcement from '../component/announce';
 
-export default function Home() {
+interface IProps {
+  rss: Parser.Output;
+}
+
+export default function Home({ rss }: IProps) {
   return (
-    <div className="app">
-      <Head>
-        <title>喵</title>
-      </Head>
-
+    <div className="schweb-home">
+      <Navbar siteTitle="總覽" />
       <Section>
-        <Container>
-          <Title>喵。</Title>
-          <Subtitle>看起來 Next.js 設定的很好。</Subtitle>
-        </Container>
+        <Columns>
+          <Column>
+            <Title>
+              We are hiring!
+            </Title>
+          </Column>
+          <Column>
+            <Announcement rss={rss} />
+          </Column>
+        </Columns>
       </Section>
     </div>
   );
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getStaticProps: GetStaticProps = async (_ctx) => {
+  const parser = new Parser();
+  const rssGuid = 'b733b7d5-2704-70be-b2c2-94180a804674';
+  const rss = await parser.parseURL(`http://www.smhs.kh.edu.tw/RssXml.php?Guid=${rssGuid}`);
+
+  return {
+    props: {
+      rss,
+    },
+  };
+};
